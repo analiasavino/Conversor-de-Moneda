f89ffae380b89789aa6bbe7317096f1a5e756a9d
Analia Savino <anuchetita@gmail.com>
2024-04-19 18:57:49 -0300

    Creo el menu de bienvenida

---
+++ src/Principal.java
@@ -0,0 +1,8 @@
+public class Principal {
+    public static void main(String[] args) {
+
+        System.out.println("***************************************");
+        System.out.println("Bienvenido a nuestro conversor de monedas");
+        System.out.println("***************************************");
+    }
+}
