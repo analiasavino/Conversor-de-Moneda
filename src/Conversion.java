1cd13dcb3bd3057b7fc691b7df16d24fd474e04f
Analia Savino <anuchetita@gmail.com>
2024-04-27 23:58:19 -0300

    cargo las nuevas clases

--- src/Conversion.java
+++ src/Conversion.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Conversion {
+import com.google.gson.Gson;
+
+public class Conversion {
+    //declaro atributos de la clase.
+    private String monedaDeOrigen;
+    private String monedaFinal;
+    private double indiceConversion;
+    private double montoAConvertir;
+    private double montoConvertido;
+
+    //creo un objeto a traves de un constructor.
+    public Conversion(String monedaDeOrigen, String monedaFinal, double indiceConversion, double montoAConvertir){
+        this.montoAConvertir = montoAConvertir;
+        this.monedaFinal = monedaFinal;
+        this.montoAConvertir = montoAConvertir;
+        this.montoConvertido = getMontoConvertido();
+    }
+
+    public Conversion(ExchangeRate exchangeRate) {
+        this.monedaDeOrigen = exchangeRate.base_code();
+        this.monedaFinal = exchangeRate.target_code();
+        this.montoConvertido = exchangeRate.conversion_result();
+        this.indiceConversion = exchangeRate.conversion_rate();
+    }
+
+    public double getMontoAConvertir() {
+       return montoAConvertir;
+    }
+
+    @Override
+    public String toString() {
+        return  " " + monedaDeOrigen +
+                " equivale a: " + montoConvertido + " " +
+                monedaFinal + "\n"
+                ;
+    }
+
+    public double getMontoConvertido() {
+        return montoConvertido;
+    }
+
+    public void setMontoAConvertir(double montoAConvertir) {
+        this.montoAConvertir = montoAConvertir;
+    }
 }
+//= ((monedaDeOrigen == "USD")? "dolares" : (monedaDeOrigen == "PYG")? "Guaranies" : (monedaDeOrigen == "ARS")?
+//                "Pesos Argentinos" : (monedaDeOrigen == "UYU")?"Pesos Uruguayos":) +
--- src/Principal.java
+++ src/Principal.java
@@ -1,8 +1,4 @@
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
+import com.google.gson.*;
 
 import java.io.IOException;
 import java.net.URI;
@@ -11,38 +7,113 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
 
+
 public class Principal {
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        System.out.println("***************************************");
+        //Declaro parametros.
+        Scanner scanner = new Scanner(System.in);
+        String menu = """
+                1 Dolar Americano   USD.
+                2 Guarani Paraguayo PYG.
+                3 Peso Argentino    ARS.
+                4 Peso Uruguayo     UYU.
+                5 Real Brasil       BRL.
+                6 Sol Peruano       PEN.
+                7 Yuan Chino        CNY. 
+                9 Para salir.
+                """;
+
+        System.out.println("\n"+ "\n" +"***************************************");
         System.out.println("Bienvenido a nuestro conversor de monedas");
-        System.out.println("***************************************");
+        System.out.println("***************************************" + '\n');
+        String monedaDeOrigen = "";
+        String monedaFinal = "";
 
-        System.out.println("""
-                Elija la conversion que desea realizar:
-                1- Pesos Argentinos a Dolares.
-                2- Dolares a Pesos Argentinos.
-                3- Reales a Dolares.
-                4- Dolares a Reales.
-                5- Pesos Uruguayos a Dolares
-                6- Dolares a Pesos Uruguayos.
-                7- Salir.
-                """);
+        while (true) {
+            System.out.println("Por favor elija en que moneda desde la que desea convertir.");
+            System.out.println(menu);
+            var opcion = scanner.nextInt();
+            if (opcion == 9)
+                break;
 
-        Scanner opcion = new Scanner(System.in);
-        var monedaElegida = opcion.nextLine();
+            switch (opcion) {
+                case 1:
+                    monedaDeOrigen = "USD";
+                    break;
+                case 2:
+                    monedaDeOrigen = "PYG";
+                    break;
+                case 3:
+                    monedaDeOrigen = "ARS";
+                    break;
+                case 4:
+                    monedaDeOrigen = "UYU";
+                    break;
+                case 5:
+                    monedaDeOrigen = "BRL";
+                    break;
+                case 7:
+                    monedaDeOrigen = "PEN";
+                    break;
+                case 8:
+                    monedaDeOrigen = "CNY";
+                    break;
+            }
+            System.out.println("Por favor elija la moneda a la cual desea convertir.");
+            var opcion2 = scanner.nextInt();
 
-        String pedido = "https://v6.exchangerate-api.com/v6/1967def449d10c81cd3d43a0/latest/"+ monedaElegida;
+            switch (opcion2) {
+                case 1:
+                    monedaFinal = "USD";
+                    break;
+                case 2:
+                    monedaFinal = "PYG";
+                    break;
+                case 3:
+                    monedaFinal = "ARS";
+                    break;
+                case 4:
+                    monedaFinal = "UYU";
+                    break;
+                case 5:
+                    monedaFinal = "BRL";
+                    break;
+                case 7:
+                    monedaFinal = "PEN";
+                    break;
+                case 8:
+                    monedaFinal = "CNY";
+                    break;
+            }
+            System.out.println("Por favor ingrese el monto que desea convertir.");
+            var montoAConvertir = scanner.nextDouble();
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request  = HttpRequest.newBuilder()
-                .uri(URI.create(pedido))
-                .build();
-        HttpResponse<String>response =client
-                .send(request, HttpResponse.BodyHandlers.ofString());
+            if (opcion == 9) {
 
-        System.out.println(response.body());
+            }
+
+            String pedido = "https://v6.exchangerate-api.com/v6/1967def449d10c81cd3d43a0/pair/" + monedaDeOrigen
+                    +"/"
+                    +monedaFinal + "/" + montoAConvertir;
+
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder() //lo que vamos a pedir
+                    .uri(URI.create(pedido))
+                    .build(); //este build se utiliza xq los objetos HttpRequest no se pueden instanciar.
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            String json = response.body();
+            Gson gson = new Gson();
+            ExchangeRate exchangeRate = gson.fromJson(json, ExchangeRate.class);
+            Conversion conversion = new Conversion(exchangeRate);
+            System.out.println("La suma de: " + montoAConvertir + conversion);
+
+
+
+
+        }
 
     }
-
 }
