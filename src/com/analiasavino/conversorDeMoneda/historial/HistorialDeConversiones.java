96dc62a5f8a88019857ea6f713b5553b88debb1f
Analia <64696388+analiasavino@users.noreply.github.com>
2024-05-15 07:13:44 -0300

    Update HistorialDeConversiones.java
    trabajando sobre el historial

--- src/com/analiasavino/conversorDeMoneda/historial/HistorialDeConversiones.java
+++ src/com/analiasavino/conversorDeMoneda/historial/HistorialDeConversiones.java
@@ -1,2 +1,23 @@
-package com.analiasavino.conversorDeMoneda.historial;public class HistorialDeConversiones {
+package com.analiasavino.conversorDeMoneda.historial;
+
+import com.analiasavino.conversorDeMoneda.modelos.Conversion;
+import com.analiasavino.conversorDeMoneda.modelos.ExchangeRate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HistorialDeConversiones extends Conversion {
+
+  public HistorialDeConversiones(String monedaDeOrigen, String monedaFinal, double indiceConversion, double montoAConvertir, double montoConvertido) {
+    super(monedaDeOrigen, monedaFinal, indiceConversion, montoAConvertir, montoConvertido);
+  }
+
+  public HistorialDeConversiones(ExchangeRate exchangeRate, double montoAConvertir) {
+    super(exchangeRate, montoAConvertir);
+  }
+
+  public HistorialDeConversiones(ExchangeRate exchangeRate) {
+    super(exchangeRate);
+  }
+
 }
